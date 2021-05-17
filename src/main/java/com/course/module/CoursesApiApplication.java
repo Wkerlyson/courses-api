cc8c9fea1e0f69b7c9a4d6cd68dec0ceb5750005
Wkerlyson Batista <wkerlyson.batista@gmail.com>
2021-05-17 15:45:38 -0300

    first commit

---
+++ src/main/java/com/course/module/CoursesApiApplication.java
@@ -0,0 +1,13 @@
+package com.course.module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursesApiApplication.class, args);
+	}
+
+}
