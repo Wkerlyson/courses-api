127283d06f9da70351128bb75b48c8005ae6cd9f
Wkerlyson Batista <wkerlyson.batista@gmail.com>
2021-05-28 19:10:05 -0300

    add lombok

--- src/main/java/com/course/module/postgres/entity/Course.java
+++ src/main/java/com/course/module/postgres/entity/Course.java
@@ -2,13 +2,29 @@ package com.course.module.postgres.entity;
 
 import java.io.Serializable;
 
+import javax.persistence.Basic;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
+import lombok.Data;
+
+@Data
 @Entity
 @Table(name = "cursos_setor", schema = "pessoal")
 public class Course implements Serializable {
 	
 	private static final long serialVersionUID = -1075419521693209775L;
+	
+	@Id
+	@Basic(optional = false)
+	@Column(name = "id_curso")
+	@SequenceGenerator(allocationSize = 1, sequenceName = "seq_curso_setor", name = "seq_curso_setor")
+	@GeneratedValue(generator = "seq_curso_setor", strategy = GenerationType.SEQUENCE)
+	private Integer idCurso;
 	
 }
--- src/main/java/com/course/module/sqlserver/entity/CourseCurriculum.java
+++ src/main/java/com/course/module/sqlserver/entity/CourseCurriculum.java
@@ -2,13 +2,22 @@ package com.course.module.sqlserver.entity;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Data;
+
+@Data
 @Entity
 @Table(name = "GradesCurriculares", schema = "dbo")
 public class CourseCurriculum implements Serializable {
 	
 	private static final long serialVersionUID = -1075419521693209775L;
+	
+	@Id
+	@Column(name = "CD_CURSO")
+	private Long id;
 	
 }
